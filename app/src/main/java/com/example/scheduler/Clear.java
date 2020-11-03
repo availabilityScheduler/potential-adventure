856978fcfae563ac70f288243d9b3a6f30ef06cf
Trung <letrung3535@gmail.com>
2020-11-02 22:12:15 -0500

    attempting to do clear through a different class...?

---
+++ app/src/main/java/com/example/scheduler/Clear.java
@@ -0,0 +1,27 @@
+package com.example.scheduler;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.RadioGroup;
+
+public class Clear extends ThirdActivity implements View.OnClickListener {
+    private RadioGroup mRadioGroup;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.app_bar_main);
+        setTitle("RadioGroupActivity");
+        mRadioGroup = (RadioGroup) findViewById(R.id.clearRG);
+        Button clearButton = (Button) findViewById(R.id.clear);
+        clearButton.setOnClickListener(this);
+    }
+
+
+
+    public void onClick(View v) {
+        mRadioGroup.clearCheck();
+    }
+}
