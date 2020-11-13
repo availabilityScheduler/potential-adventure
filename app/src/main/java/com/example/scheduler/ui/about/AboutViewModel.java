79d2dc6a19484fe09171b13529162f15a3963c16
Jonathan <jonathanharksen@gmail.com>
2020-11-12 22:26:11 -0500

    Changing the friend search menu to open from the bottom

--- app/src/main/java/com/example/scheduler/ui/about/AboutViewModel.java
+++ app/src/main/java/com/example/scheduler/ui/about/AboutViewModel.java
@@ -1,14 +1,14 @@
-package com.example.scheduler.ui.gallery;
+package com.example.scheduler.ui.about;
 
 import androidx.lifecycle.LiveData;
 import androidx.lifecycle.MutableLiveData;
 import androidx.lifecycle.ViewModel;
 
-public class GalleryViewModel extends ViewModel {
+public class AboutViewModel extends ViewModel {
 
     private MutableLiveData<String> mText;
 
-    public GalleryViewModel() {
+    public AboutViewModel() {
         mText = new MutableLiveData<>();
         mText.setValue("This is gallery fragment");
     }
