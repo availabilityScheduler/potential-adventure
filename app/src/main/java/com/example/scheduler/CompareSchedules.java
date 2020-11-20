e24d8b9b02730cd768b596dbe77ccdeb16d334c1
David Bittner <david.r.bittner@gmail.com>
2020-11-20 16:58:23 -0500

    cant get back button to work

---
+++ app/src/main/java/com/example/scheduler/CompareSchedules.java
@@ -0,0 +1,19 @@
+package com.example.scheduler;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.Button;
+
+public class CompareSchedules extends AppCompatActivity {
+
+    private Button backButton;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_compare_schedules);
+
+        //backButton = findViewById(R.id.backButton);
+    }
+}
