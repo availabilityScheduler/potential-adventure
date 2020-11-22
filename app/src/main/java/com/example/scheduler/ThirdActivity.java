07a8d83ddcd6d711b2eadee0ab88464f96850296
mahir <mmahir380@gmail.com>
2020-11-22 13:43:50 -0500

    defaults at home fragment

--- app/src/main/java/com/example/scheduler/ThirdActivity.java
+++ app/src/main/java/com/example/scheduler/ThirdActivity.java
@@ -1,6 +1,7 @@
 package com.example.scheduler;
 
 import android.annotation.SuppressLint;
+import android.app.FragmentTransaction;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.preference.PreferenceManager;
@@ -46,6 +47,7 @@ import androidx.annotation.NonNull;
 import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.core.view.GravityCompat;
 import androidx.fragment.app.DialogFragment;
+import androidx.fragment.app.Fragment;
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
 import androidx.navigation.ui.AppBarConfiguration;
@@ -187,6 +189,11 @@ public class ThirdActivity extends AppCompatActivity implements NavigationView.O
             db.child(userAuthId).setValue(thisMember);
         }
 
+        if (savedInstanceState == null) {
+            getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
+                    new HomeFragment()).commit();
+            mNavigationView.setCheckedItem(R.id.nav_home);
+        }
         //Ends onCreate()
     }
 
@@ -232,5 +239,14 @@ public class ThirdActivity extends AppCompatActivity implements NavigationView.O
         }
         drawer.closeDrawer(GravityCompat.START);
         return true;
+    }
+
+    @Override
+    public void onBackPressed() {
+        if (drawer.isDrawerOpen(GravityCompat.START)) {
+            drawer.closeDrawer(GravityCompat.START);
+        } else {
+            super.onBackPressed();
+        }
     }
 }
