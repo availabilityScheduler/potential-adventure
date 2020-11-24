6dd09f335656ebe82b7e96d37b45576968d215ea
original_hiro <mmahir380@gmail.com>
2020-11-23 23:36:24 -0500

    revenue hehehe

--- app/src/main/java/com/example/scheduler/fragments/AboutFragment.java
+++ app/src/main/java/com/example/scheduler/fragments/AboutFragment.java
@@ -10,12 +10,15 @@ import android.view.View;
 import android.view.ViewGroup;
 
 import com.example.scheduler.R;
+import com.google.android.gms.ads.MobileAds;
 
 public class AboutFragment extends Fragment {
 
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        MobileAds.initialize(getContext());
+
         return inflater.inflate(R.layout.fragment_about, container, false);
     }
 }
