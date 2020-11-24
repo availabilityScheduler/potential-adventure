2e8d1050dcc82206ba382b26de9ca59b8fa839c0
original_hiro <mmahir380@gmail.com>
2020-11-24 00:31:20 -0500

    ads

--- app/src/main/java/com/example/scheduler/fragments/AboutFragment.java
+++ app/src/main/java/com/example/scheduler/fragments/AboutFragment.java
@@ -10,15 +10,25 @@ import android.view.View;
 import android.view.ViewGroup;
 
 import com.example.scheduler.R;
+import com.google.android.gms.ads.AdRequest;
+import com.google.android.gms.ads.AdView;
 import com.google.android.gms.ads.MobileAds;
 
 public class AboutFragment extends Fragment {
 
+    private AdView adview1;
+
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        MobileAds.initialize(getContext());
+        View v = inflater.inflate(R.layout.fragment_home, container, false);
 
+//        adview1 = v.findViewById(R.id.adView);
+//        MobileAds.initialize(getContext());
+//        AdRequest adRequest = new AdRequest.Builder().build();
+//        adview1.loadAd(adRequest);
         return inflater.inflate(R.layout.fragment_about, container, false);
     }
+
+
 }
--- app/src/main/java/com/example/scheduler/social/UserProfile.java
+++ app/src/main/java/com/example/scheduler/social/UserProfile.java
@@ -12,13 +12,24 @@ import android.widget.TextView;
 
 import com.example.scheduler.mainActivities.Member;
 import com.example.scheduler.R;
+import com.google.android.gms.ads.AdRequest;
+import com.google.android.gms.ads.AdView;
+import com.google.android.gms.ads.MobileAds;
+import com.google.android.gms.ads.RequestConfiguration;
+import com.google.android.gms.ads.initialization.InitializationStatus;
+import com.google.android.gms.ads.initialization.OnInitializationCompleteListener;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
+
+import java.util.Arrays;
 
 public class UserProfile extends AppCompatActivity {
 
     private TextView userName;
     private Member thisUser;
     private FloatingActionButton backbtn;
+
+    private AdView adview1;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -36,6 +47,29 @@ public class UserProfile extends AppCompatActivity {
             }
         });
 
+        // Initialize the Mobile Ads SDK.
+        MobileAds.initialize(this, new OnInitializationCompleteListener() {
+            @Override
+            public void onInitializationComplete(InitializationStatus initializationStatus) {}
+        });
+
+        // Set your test devices. Check your logcat output for the hashed device ID to
+        // get test ads on a physical device. e.g.
+        // "Use RequestConfiguration.Builder().setTestDeviceIds(Arrays.asList("ABCDEF012345"))
+        // to get test ads on this device."
+        MobileAds.setRequestConfiguration(
+                new RequestConfiguration.Builder().setTestDeviceIds(Arrays.asList("ABCDEF012345"))
+                        .build());
+
+        // Gets the ad view defined in layout/ad_fragment.xml with ad unit ID set in
+        // values/strings.xml.
+        adview1 = findViewById(R.id.adView);
+
+        // Create an ad request.
+        AdRequest adRequest = new AdRequest.Builder().build();
+
+        // Start loading the ad in the background.
+        adview1.loadAd(adRequest);
 
         //GoogleSignInAccount acct = GoogleSignIn.getAccountForExtension();
 
